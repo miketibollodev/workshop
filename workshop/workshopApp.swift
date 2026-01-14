36f182f4d45a4fa8fd9c38c89895f21200e33a15
Mike Tibollo <miketibollodev@gmail.com>
2026-01-14 15:15:03 -0600

    Initial commit

---
+++ workshop/workshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  workshopApp.swift
+//  workshop
+//
+//  Created by Michael Tibollo on 2026-01-14.
+//
+
+import SwiftUI
+
+@main
+struct workshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ workshopTests/workshopTests.swift
@@ -0,0 +1,17 @@
+//
+//  workshopTests.swift
+//  workshopTests
+//
+//  Created by Michael Tibollo on 2026-01-14.
+//
+
+import Testing
+@testable import workshop
+
+struct workshopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
