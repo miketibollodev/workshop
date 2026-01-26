69da661e610b50b9b9df05d27061d116689beb87
Mike Tibollo <miketibollodev@gmail.com>
2026-01-26 12:09:59 -0600

    Simplified IconView and removed Icon enum

--- Packages/Components/Sources/Components/Images/IconView.swift
+++ Packages/Components/Sources/Components/Images/IconView.swift
@@ -7,40 +7,26 @@
 
 import SwiftUI
 
-public enum IconSize: Identifiable {
-    public var id: Self { self }
-    
-    case small
-    case medium
-    case large
-    
-    var size: CGSize {
-        switch self {
-        case .small: CGSize(width: 12.0, height: 12.0)
-        case .medium: CGSize(width: 16.0, height: 16.0)
-        case .large: CGSize(width: 24.0, height: 24.0)
+public struct IconView: View {
+    internal enum IconSize {
+        case small, medium, large
+        
+        var size: CGSize {
+            switch self {
+            case .small: CGSize(width: 12.0, height: 12.0)
+            case .medium: CGSize(width: 16.0, height: 16.0)
+            case .large: CGSize(width: 24.0, height: 24.0)
+            }
         }
     }
-}
-
-public struct IconView: View {
     
     var icon: ImageResource
-    var iconSize: IconSize
-    
-    public init(_ icon: ImageResource, iconSize: IconSize) {
-        self.icon = icon
-        self.iconSize = iconSize
-    }
+    var size: IconSize
     
     public var body: some View {
         Image(icon)
             .resizable()
             .scaledToFit()
-            .frame(iconSize.size)
+            .frame(size.size)
     }
-}
-
-#Preview {
-    IconView(Icon.Filled.Navigation.arrowLeft, iconSize: .small)
 }
